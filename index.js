c852c0a4ce68b32586b2f188f97682c46946ede2
Mehmet Ali Özdemir <slsh585@gmail.com>
2021-03-15 23:01:28 +0300

    Make the appearance of the login page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const { mongoose } = require('./bootstrap')
@@ -8,6 +9,8 @@ const accountRoute = require('./routes/account')
 
 app.set('view engine', 'pug')
 app.set('views','views')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use('/', indexRoute)
