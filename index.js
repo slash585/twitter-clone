d12d4e828ecd822256536fe105309109ee7a6a0a
Mehmet Ali Özdemir <slsh585@gmail.com>
2021-03-16 00:04:30 +0300

    Check for empty fields

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express()
 const { mongoose } = require('./bootstrap')
@@ -10,8 +11,8 @@ const accountRoute = require('./routes/account')
 app.set('view engine', 'pug')
 app.set('views','views')
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
-
 
 app.use('/', indexRoute)
 app.use('/account', accountRoute)
--- routes/account.js
+++ routes/account.js
@@ -8,4 +8,21 @@ router.get('/register', (req,res,next)=>{
     res.status(200).render('register')
 })
 
+router.post('/register', async(req,res,next)=>{
+    const firstName = req.body.firstName.trim()
+    const lastName = req.body.lastName.trim()
+    const username = req.body.username.trim()
+    const email = req.body.email.trim()
+    const password = req.body.password.trim()
+
+    const payload = req.body
+
+    if(firstName && lastName && username && email && password){
+
+    }else{
+        payload.errorMessage = 'Make sure each has a valid value.'
+        res.status(200).render('register', payload)
+    }
+})
+
 module.exports = router
