6a3cb3b4a2b5736e1df7a28545be14d9f84e646a
Mehmet Ali Özdemir <slsh585@gmail.com>
2021-03-16 00:43:49 +0300

    Add timestamp to user model

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-const UserSchema = new mongoose.Schema({
-    firstName: {type: String, required: true, trim: true},
-    lastName: {type: String, required: true, trim: true},
-    username: {type: String, required: true, trim: true, unique: true},
-    email: {type: String, required: true, trim: true, unique: true},
-    password: {type: String, required: true},
-    profilePicture: {type: String, default: '/images/profilePictures.jpg'}
-})
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    username: { type: String, required: true, trim: true, unique: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String, default: "/images/profilePictures.jpg" },
+  },
+  { timestamps: true }
+)
 
-module.exports = mongoose.model('User',UserSchema)
+module.exports = mongoose.model("User", UserSchema)
