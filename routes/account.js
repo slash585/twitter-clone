ff7ef31d0a1abc4d9dbf006d790b1bfed508d46d
Mehmet Ali Özdemir <slsh585@gmail.com>
2021-03-15 23:34:52 +0300

    Check that the passwords match

--- routes/account.js
+++ routes/account.js
@@ -4,4 +4,8 @@ router.get('/login', (req,res,next)=>{
     res.status(200).render('login')
 })
 
+router.get('/register', (req,res,next)=>{
+    res.status(200).render('register')
+})
+
 module.exports = router
